232cf7ed66b20afc7ccf467b81afcea64ad10045
GBinLabs <197016998+GBinLabs@users.noreply.github.com>
2025-07-07 18:09:50 -0300

    Tratando de configurar Dorion

--- Home-manager/Aplicaciones/Aplicaciones/Discord/Discord.nix
+++ Home-manager/Aplicaciones/Aplicaciones/Discord/Discord.nix
@@ -1,9 +1,15 @@
-{...}:
-
-{
+{pkgs, ...}: {
   programs.nixcord = {
     enable = true;
-    discord.enable = false;
-    vesktop.enable = true;
+    discord = {
+      enable = false;
+    };
+    vesktop = {
+      enable = false;
+    };
+    dorion = {
+      enable = true;
+      package = pkgs.dorion;
+    };
   };
 }
--- Home-manager/Aplicaciones/Aplicaciones/default.nix
+++ Home-manager/Aplicaciones/Aplicaciones/default.nix
@@ -1,6 +1,7 @@
 _: {
   imports = [
     ./Aplicaciones/Aplicaciones.nix
+    ./Discord/Discord.nix
     ./Editores/default.nix
     ./Gestor-archivos/default.nix
     ./Git/default.nix
