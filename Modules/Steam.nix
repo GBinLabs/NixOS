2cd5bf065113899473dcc1fad392bd1bb4136ef3
GBinLabs <197016998+GBinLabs@users.noreply.github.com>
2026-01-25 20:26:12 -0300

    Actualizacion diaria - 25 de Enero de 2026

--- Modules/Steam.nix
+++ Modules/Steam.nix
@@ -65,27 +65,12 @@ in
       enable = true;
       platformOptimizations.enable = true;
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
-      package = pkgs.steam.override {
-        extraPkgs =
-          pkgs': with pkgs'; [
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            libkrb5
-            keyutils
-          ];
-      };
       gamescopeSession.enable = true;
     };
 
     programs.gamescope = {
       enable = true;
-      capSysNice = true;
+      capSysNice = false;
     };
 
     programs.gamemode = {
