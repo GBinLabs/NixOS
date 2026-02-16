a90f5f89a0ce4656b0b86e0588563daa625a8c99
GBinLabs <197016998+GBinLabs@users.noreply.github.com>
2026-02-15 21:09:45 -0300

    2 ZED

--- Home-manager/Aplicaciones/Editores/Zed/Zed.nix
+++ Home-manager/Aplicaciones/Editores/Zed/Zed.nix
@@ -1,23 +1,18 @@
 { pkgs, ... }:
-
 {
-  home.packages = with pkgs; [
-    nixd
-    nixfmt
-    texliveFull
-    texlab
-    pdf2svg
-    ghostscript
-    typos-lsp
-  ];
+  home = {
+    packages = with pkgs; [
+      nixd
+      nixfmt
+    ];
+    sessionVariables.ZED_WINDOW_DECORATIONS = "server";
+  };
 
   programs.zed-editor = {
     enable = true;
-    package = pkgs.zed-editor;
-
     extensions = [
       "nix"
-      "latex"
+      "typst"
       "toml"
       "json"
       "typos"
@@ -30,191 +25,42 @@
         diagnostics = false;
         metrics = false;
       };
+      features.edit_prediction_provider = "none";
+      agent.enabled = false;
 
-      # Tema - usar el nombre exacto generado por Noctalia
       theme = {
         mode = "dark";
         dark = "Noctalia dark transparent";
         light = "Noctalia light";
       };
 
-      experimental = {
-        theme_overrides = {
-          "background" = "#00000000";
-          "editor.background" = "#00000000";
-          "background.appearance" = "transparent";
-        };
+      theme_overrides."Noctalia dark transparent" = {
+        "background" = "#00000000";
+        "editor.background" = "#00000000";
+        "tab_bar.background" = "#00000000";
+        "toolbar.background" = "#00000000";
+        "panel.background" = "#00000000";
+        "status_bar.background" = "#00000000";
+        "title_bar.background" = "#00000000";
+        "background.appearance" = "transparent";
       };
 
-      ui_font_size = 14;
-      ui_font_family = "Inter";
-      buffer_font_size = 14;
       buffer_font_family = "JetBrains Mono";
-      buffer_font_features = {
-        calt = true;
-        liga = true;
-      };
-
-      tab_size = 2;
-      hard_tabs = false;
-      show_whitespaces = "boundary";
-      wrap_guides = [
-        80
-        100
-      ];
-      soft_wrap = "editor_width";
-      preferred_line_length = 100;
-      format_on_save = "on";
-      cursor_blink = false;
-
-      scrollbar = {
-        show = "auto";
-      };
-
-      gutter = {
-        line_numbers = true;
-        code_actions = true;
-        folds = true;
-      };
-
-      indent_guides = {
-        enabled = true;
-        coloring = "indent_aware";
-      };
-
-      inlay_hints = {
-        enabled = true;
-      };
-
-      autosave = "off";
-
-      toolbar = {
-        breadcrumbs = true;
-        quick_actions = true;
-        selections_menu = true;
-      };
-
-      tabs = {
-        close_position = "right";
-        file_icons = true;
-        git_status = true;
-      };
-
-      project_panel = {
-        dock = "left";
-        git_status = true;
-      };
-
-      git = {
-        git_gutter = "tracked_files";
-        inline_blame = {
-          enabled = true;
-        };
-      };
-
-      terminal = {
-        shell = {
-          program = "zsh";
-        };
-        font_family = "JetBrains Mono";
-        font_size = 14;
-      };
-
-      file_scan_exclusions = [
-        "**/.git"
-        "**/node_modules"
-        "**/result"
-        "**/*.aux"
-        "**/*.bbl"
-        "**/*.blg"
-        "**/*.fdb_latexmk"
-        "**/*.fls"
-        "**/*.log"
-        "**/*.out"
-        "**/*.synctex.gz"
-        "**/*.toc"
-      ];
-
-      # IA desactivada
-      features = {
-        edit_prediction_provider = "none";
-      };
-
-      agent = {
-        enabled = false;
-      };
-
-      hour_format = "hour24";
+      terminal.font_family = "JetBrains Mono";
 
       languages = {
-        Nix = {
-          language_servers = [ "nixd" ];
-          formatter = {
-            external = {
-              command = "nixfmt";
-              arguments = [ ];
-            };
-          };
-          tab_size = 2;
-          format_on_save = "on";
-        };
-        LaTeX = {
-          language_servers = [
-            "texlab"
-            "ltex"
-          ];
-          tab_size = 2;
-          soft_wrap = "editor_width";
-          format_on_save = "off";
-        };
-        Markdown = {
-          language_servers = [ "ltex" ];
-        };
-        BibTeX = {
-          language_servers = [ "ltex" ];
-        };
+        Nix.language_servers = [ "nixd" ];
+        Nix.formatter.external.command = "nixfmt";
+        Typst.language_servers = [
+          "tinymist"
+          "ltex"
+        ];
+        Markdown.language_servers = [ "ltex" ];
       };
 
       lsp = {
-        nixd = {
-          binary = {
-            path_lookup = true;
-          };
-        };
-        texlab = {
-          binary = {
-            path_lookup = true;
-          };
-          settings = {
-            texlab = {
-              build = {
-                onSave = true;
-                forwardSearchAfter = true;
-              };
-              chktex = {
-                onOpenAndSave = true;
-              };
-            };
-          };
-        };
-        typos = {
-          binary = {
-            path_lookup = true;
-          };
-        };
-        ltex = {
-          settings = {
-            ltex = {
-              language = "es-AR";
-              additionalRules = {
-                motherTongue = "es-AR";
-                enablePickyRules = true;
-              };
-              completionEnabled = true;
-              diagnosticSeverity = "information";
-            };
-          };
-        };
+        tinymist.settings.exportPdf = "onSave";
+        ltex.settings.ltex.language = "es-AR";
       };
     };
   };
